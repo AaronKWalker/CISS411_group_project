c18f9cdcca1ba7ce6f4240b8b69ca0097b1225dc
Walker <aaron.walker@accruent.com>
2020-06-15 17:25:49 -0500

    added SessionTitle and LessonTitle to proper models created M3 migration

---
+++ GroupProjectCISS411/Migrations/20200615222256_M3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GroupProjectCISS411.Migrations
+{
+    public partial class M3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SessionTitle",
+                table: "Sessions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LessonTitle",
+                table: "Lessons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionTitle",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "LessonTitle",
+                table: "Lessons");
+        }
+    }
+}
